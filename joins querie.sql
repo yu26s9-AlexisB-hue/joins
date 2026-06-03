39db3606ee73b44036e837ca2a43a2895043e69e
Alexis Barrett <happynessb09@gmail.com>
2026-06-03 12:44:50 -0400

    finshed 2nd question

--- joins querie.sql
+++ joins querie.sql
@@ -1,7 +1,18 @@
 -- Joins
 use northwind;
 
+/*
+
 select products.productID, products.ProductName, products.UnitPrice, Categories.categoryName
 from products
 join categories on products.CategoryID = categories.CategoryID
 order by categoryName, productName;
+
+*/
+
+select products.productId, products.ProductName,
+ products.unitprice,suppliers.ContactName
+ from products
+ join suppliers on products.supplierID = suppliers.SupplierID
+ where UnitPrice > 75
+ order by ProductName;
